a0a2808a0bc048732812f5a35bc3f63965cfec67
Agustin Caceres <sebaxt10@outlook.com>
2022-07-18 09:32:11 -0300

    ejercicio10

--- js/proyectojs10.js
+++ js/proyectojs10.js
@@ -1,22 +1,22 @@
-const filas = parseInt(prompt('Ingrese el numero de filas'));
-const columnas = parseInt(prompt('Ingrese un numero de columnas'));
+const filas = parseInt(prompt("Ingrese el numero de filas"));
+const columnas = parseInt(prompt("Ingrese un numero de columnas"));
 let resultado = filas * columnas;
 
 console.log(filas);
 console.log(columnas);
 
-    document.write(`<table>
-    <tbody>`)
+document.write(`<table>
+    <tbody>`);
 
-    for(let inidiceFila = 0; inidiceFila < filas; inidiceFila++ ){
-        document.write(`<tr>`)
-        for(let indiceColumna = 0; indiceColumna < columnas; indiceColumna++){
-            document.write(`<td>${resultado}</td>`);
-            resultado--; //resultado = resultado - 1
-        }
-        document.write(`</tr>`)
-    }
+for (let inidiceFila = 0; inidiceFila < filas; inidiceFila++) {
+  document.write(`<tr>`);
+  for (let indiceColumna = 0; indiceColumna < columnas; indiceColumna++) {
+    document.write(`<td>${resultado}</td>`);
+    resultado--;
+  }
+  document.write(`</tr>`);
+}
 
-    document.write(`
+document.write(`
     </tbody>
-    </table>`)
+    </table>`);
