f302dc4f849381fa3cf962793ae9366f8cc9ac3f
Agustin Caceres <sebaxt10@outlook.com>
2022-07-18 09:29:12 -0300

    ejercicio 10

---
+++ js/proyectojs10.js
@@ -0,0 +1,22 @@
+const filas = parseInt(prompt('Ingrese el numero de filas'));
+const columnas = parseInt(prompt('Ingrese un numero de columnas'));
+let resultado = filas * columnas;
+
+console.log(filas);
+console.log(columnas);
+
+    document.write(`<table>
+    <tbody>`)
+
+    for(let inidiceFila = 0; inidiceFila < filas; inidiceFila++ ){
+        document.write(`<tr>`)
+        for(let indiceColumna = 0; indiceColumna < columnas; indiceColumna++){
+            document.write(`<td>${resultado}</td>`);
+            resultado--; //resultado = resultado - 1
+        }
+        document.write(`</tr>`)
+    }
+
+    document.write(`
+    </tbody>
+    </table>`)
---
+++ js/proyectojs7.js
@@ -0,0 +1,9 @@
+let numero = prompt("ingrese su numero")
+
+while(numero >30){
+    numero --;
+    for(let i=0;i<numero;i++){
+        document.write(numero)
+        
+    }document.write("</br>")
+}
